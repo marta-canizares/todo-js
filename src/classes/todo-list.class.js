ecb39d01b4cac7a01526e6e90d02f26c258c9933
marta <marta.s.canizares@gmail.com>
2020-11-07 17:38:18 +0100

    first commit

---
+++ src/classes/todo-list.class.js
@@ -0,0 +1,56 @@
+import { Todo } from './todo.class';
+
+export class TodoList {
+
+
+    constructor() {
+        //  this.todos = [];
+        this.cargarLocalStorage();
+
+    }
+
+
+    nuevoTodo(todo) {
+        this.todos.push(todo);
+    }
+
+    eliminarTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id != id);
+        this.guardarLocalStorage();
+
+    }
+
+    marcarCompletado(id) {
+
+        for (let todo of this.todos) {
+            if (todo.i == id) {
+                todo.completado = !todo.completado
+                this.guardarLocalStorage();
+                break;
+            }
+        }
+
+    }
+
+    eliminarCompletados() {
+        this.todos = this.todos.filter(todo => !todo.completado);
+        this.guardarLocalStorage();
+    }
+
+
+    guardarLocalStorage() {
+
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+
+    }
+
+    cargarLocalStorage() {
+
+        this.todos = (localStorage.getItem('todo'))
+            ? JSON.parse(localStorage.getItem('todo'))
+            : [];
+
+        this.todos = this.todos.map(Todo.fromJson);
+    }
+
+}
